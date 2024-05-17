418d6cf18f54ce0a7e8d9df5f750b88987c99e52
ReginaAltubias <reginaaltubias@gmail.com>
2024-05-17 11:42:58 +0100

    Primeiro commit

--- components/javascript.js
+++ components/javascript.js
@@ -2,6 +2,7 @@ let timer;
 let hours = 0;
 let minutes = 0; //Contadores para armazenar o tempo
 let seconds = 0;
+let isStarted = false;
 
 //Atualiza os elementos com os valores atuais de hours, minutes e seconds.
 //Utilize `StrinString.padStart(2, '0') para garantir que os valores tenham sempre dois dígitos.
@@ -29,14 +30,21 @@ function startTimer() {
             }
             updateDisplay();
         }, 1000);
+        isStarted = true;
+        document.getElementById('stopButton').disabled = false;
     }
 }
-
 //Para o temporizador limpando o intervalo apenas se ele estiver rodando (if (timer)).
 function stopTimer() {
-    if (timer) {
-        clearInterval(timer);
-        timer = null;
+    if (isStarted) {
+        if (timer) {
+            clearInterval(timer);
+            timer = null;
+            document.getElementById('stopButton').textContent = 'Continuar';
+        } else {
+            startTimer();
+            document.getElementById('stopButton').textContent = 'Parar';
+        }
     }
 }
 
@@ -46,6 +54,9 @@ function resetTimer() {
     minutes = 0;
     seconds = 0;
     updateDisplay();
+    document.getElementById('stopButton').textContent = 'Parar';
+    document.getElementById('stopButton').disabled = true;
+    isStarted = false;
 }
 
 document.getElementById('startButton').addEventListener('click', startTimer);
